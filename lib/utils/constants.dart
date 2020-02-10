9cb65ba1109f411d05bac1dd5d14649f2a41512e
Amir Hilmi <amir.hi575@gmail.com>
2020-02-10 11:48:56 +0800

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+
+
+class Constants {
+
+    static final String HEADLINE_NEWS_URL = 'https://newsapi.org/v2/everything?q=islam&apiKey=f050ceeea82c4c2a995f6633ec50165b';
+
+    static final String NEWS_PLACEHOLDER_IMAGE_ASSET_URL = 'assets/placeholder.png';
+
+    
+
+}

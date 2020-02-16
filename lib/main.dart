51758d381c79b7caca6a0ed178e4ad99f72471b2
Amir Hilmi <amir.hi575@gmail.com>
2020-02-16 22:21:29 +0800

    working my masjid app that has carousell, newsfeed, and app list. Should function as resetLine

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:networking_demo/widgets/newsList.dart';
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1537181534458-45dcee76ae90?ixlib=rb-1.2.1&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1574246604907-db69e30ddb97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
 
 void main() {
   runApp(MaterialApp(
@@ -12,10 +23,112 @@ void main() {
   ));
 }
 
+//start carousell
+
+final Widget placeholder = Container(color: Colors.grey);
+
+final List child = map<Widget>(
+  imgList,
+  (index, i) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(children: <Widget>[
+          Image.network(i, fit: BoxFit.cover, width: 1000.0),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                'No. $index image',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  },
+).toList();
+
+List<T> map<T>(List list, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(handler(i, list[i]));
+  }
+
+  return result;
+}
+
+class CarouselWithIndicator extends StatefulWidget {
+  @override
+  _CarouselWithIndicatorState createState() => _CarouselWithIndicatorState();
+}
+
+class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
+  int _current = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      CarouselSlider(
+        items: child,
+        autoPlay: true,
+        enlargeCenterPage: true,
+        aspectRatio: 2.0,
+        onPageChanged: (index) {
+          setState(() {
+            _current = index;
+          });
+        },
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: map<Widget>(
+          imgList,
+          (index, url) {
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index
+                      ? Color.fromRGBO(0, 0, 0, 0.9)
+                      : Color.fromRGBO(0, 0, 0, 0.4)),
+            );
+          },
+        ),
+      ),
+    ]);
+  }
+}
+
+//end of carousell
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
+
+    
+    
+    
     return new MaterialApp(
       title: 'MediaQuery Demo',
       theme: new ThemeData(
@@ -23,6 +136,9 @@ class MyApp extends StatelessWidget {
       ),
       home: new MyHomePage(),
     );
+
+    
+  
   }
 }
 
@@ -36,6 +152,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+
+    final CarouselSlider manualCarouselDemo = CarouselSlider(
+      items: child,
+      autoPlay: false,
+      enlargeCenterPage: true,
+      viewportFraction: 0.9,
+      aspectRatio: 2.0,
+    );    
+
+
       Paint paintRed = Paint();
       paintRed.color = Colors.red;
       Paint paintYellow = Paint();
@@ -221,7 +347,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
               child: new Container(
-                height: 100.0,
+                height: 300.0,
                 width: MediaQuery
                     .of(context)
                     .size
@@ -238,19 +364,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
                   child: Container(
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          //begin: Alignment.topRight,
-                          //end: Alignment.bottomLeft,
-                            colors: [
-                              Colors.amberAccent,
-                              Colors.amberAccent,
-                              Colors.amber[100]
-                            ]
+                    // decoration: BoxDecoration(
+                    //     gradient: LinearGradient(
+                    //       //begin: Alignment.topRight,
+                    //       //end: Alignment.bottomLeft,
+                    //         colors: [
+                    //           Colors.amberAccent,
+                    //           Colors.amberAccent,
+                    //           Colors.amber[100]
+                    //         ]
 
-                        )
+                    //     )
 
-                    ),
+                    // ),
 
 
                     //elevation: 4.0,
@@ -266,19 +392,32 @@ class _MyHomePageState extends State<MyHomePage> {
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
 
+                              Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                                  child: Column(children: [
+
+                                       Text(
+                                      'Highlight : Newsfeed',
+                                      
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                      
+                                      
+
+                                ),
+                              ),
+                                    CarouselWithIndicator(),
+                                  ])),
+           
+
                               SizedBox(
                                 height: 35.0,
                               ),
+                              
 
-                              Text(
-                                'CONTENTS',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-
-                                ),
-                              )
                             ],
                           ),
                         ),
